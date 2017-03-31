d2a40c12e3e939748ea94031b1bf899172e34000
Jan Rybka <janry@netwise.pl>
2017-03-31 13:59:10 +0200

    Drobne poprawki

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { PageScrollService, PageScrollInstance, PageScrollConfig } from "ng2-page-scroll/ng2-page-scroll";
+import { DOCUMENT } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  
+  public constructor(private pageScrollService: PageScrollService, @Inject(DOCUMENT) private document: any) {
+    PageScrollConfig.defaultDuration = 300;
+  }
+
+  navigate(anchor: string): void {
+    if (!anchor) { return; }
+    const pageScrollInstance: PageScrollInstance = PageScrollInstance.simpleInstance(this.document, anchor);
+    this.pageScrollService.start(pageScrollInstance);
+  }
 }
